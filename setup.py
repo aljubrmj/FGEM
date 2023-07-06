b8e4e7387ede8d29f93c8b5124ee6fd53b4ca1be
Mohammad (Jabs) <44715677+aljubrmj@users.noreply.github.com>
2023-07-06 16:13:11 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from fgem import __version__
+
+setup(
+    name='fgem',
+    version=__version__,
+
+    url='https://github.com/aljubrmj/FGEM',
+    author='Mohammad Aljubran',
+    author_email='aljubrmj@gmail.com; aljubrmj@stanford.edu',
+
+    py_modules=['fgem'],
+)
