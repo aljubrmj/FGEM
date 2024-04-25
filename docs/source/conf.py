f717fe3ef6c448981ac288abd688121e2d838ba3
Mohammad (Jabs) <44715677+aljubrmj@users.noreply.github.com>
2024-04-25 14:26:43 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,11 +12,20 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    #'sphinx.ext.autodoc',
+    #'sphinx.ext.autosummary',
+    "myst_parser",
+    # "sphinx.ext.autosectionlabel",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.graphviz",
+    # "sphinxcontrib.bibtex",
+    #'sphinx.ext.pngmath',
+    #'sphinxcontrib.tikz',
+    #'rinoh.frontend.sphinx',
+    "sphinx.ext.imgconverter",  # for SVG conversion
 ]
 
 intersphinx_mapping = {
@@ -29,7 +38,25 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
+
+html_theme_options = {
+    "repository_url": "https://github.com/aljubrmj/FGEM",
+    "use_repository_button": True,
+    "show_navbar_depth": 1,
+}
+
+# The name for this set of Sphinx documents.  If None, it defaults to
+# "<project> v<release> documentation".
+# html_title = "FGEM"
+
+# A shorter title for the navigation bar.  Default is the same as html_title.
+# html_short_title = "FGEM"
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = "./_static/fgem-logo.png"
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'
+# epub_show_urls = 'footnote'
