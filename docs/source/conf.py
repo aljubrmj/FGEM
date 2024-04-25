65a763dc12dcbb69fdc624d964b37b194e1aa1cb
Mohammad (Jabs) <44715677+aljubrmj@users.noreply.github.com>
2024-04-25 13:25:09 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FGEM'
+copyright = '2024, Mohammad Aljubran'
+author = 'Mohammad Aljubran'
 
 release = '0.1'
 version = '0.1.0'
