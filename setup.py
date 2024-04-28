c3220044dd40fd896069f680130f2469c9895732
Mohammad (Jabs) <44715677+aljubrmj@users.noreply.github.com>
2024-04-28 12:43:55 -0700

    .

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup
-from pip.req import parse_requirements
 from fgem import __version__
-
-install_reqs = parse_requirements("requirements.txt")
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='fgem',
@@ -12,6 +8,19 @@ setup(
     url='https://github.com/aljubrmj/FGEM',
     author='Mohammad Aljubran',
     author_email='aljubrmj@gmail.com; aljubrmj@stanford.edu',
-    install_requires=reqs,
+
+    install_requires=[
+                    "matplotlib==3.6.3",
+                    "numpy==1.25.2",
+                    "pandas==1.5.2",
+                    "pyXSteam==0.4.9",
+                    "scipy==1.11.3",
+                    "seaborn==0.12.2",
+                    "Shapely==2.0.1",
+                    "tqdm==4.64.1",
+                    "numpy-financial==1.0.0",
+                    "timezonefinder==6.2.0",
+                    "meteostat==1.6.5"],
+
     py_modules=['fgem'],
 )
