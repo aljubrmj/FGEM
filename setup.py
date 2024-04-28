24d0e7fc950fed56dd27c1d90e3bcf101dbd216e
Mohammad (Jabs) <44715677+aljubrmj@users.noreply.github.com>
2024-04-28 12:39:20 -0700

    .

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
-
+from pip.req import parse_requirements
 from fgem import __version__
+
+install_reqs = parse_requirements("requirements.txt")
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='fgem',
@@ -9,6 +12,6 @@ setup(
     url='https://github.com/aljubrmj/FGEM',
     author='Mohammad Aljubran',
     author_email='aljubrmj@gmail.com; aljubrmj@stanford.edu',
-
+    install_requires=reqs,
     py_modules=['fgem'],
 )
